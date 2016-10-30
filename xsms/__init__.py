341ebedd44b778a6a7e3304c0ec17c743ccd5659
z <z@xnz.me>
2016-10-30 14:18:00 -0400

    only add config to __all__

--- xsms/__init__.py
+++ xsms/__init__.py
@@ -2,14 +2,7 @@ from .__about__ import (__title__, __package_name__, __version__, __summary__, _
                         __keywords__, __email__, __author__, __license__, __copyright__)
 
 from . import config
-from . import util
-from . import servers
-from . import cli
-
 
 __all__ = [
     'config',
-    'util',
-    'servers',
-    'cli',
 ]
